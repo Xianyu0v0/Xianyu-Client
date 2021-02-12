3b29578311e649026433160a582c6b7ea897a285
ImXianyu <2030752250@qq.com>
2021-02-12 18:06:27 +0800

    First commit to All.

--- src/xyz/xianyu/Main.java
+++ src/xyz/xianyu/Main.java
@@ -7,6 +7,6 @@ package xyz.xianyu;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");;
     }
 }
